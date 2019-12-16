a841431cf33d506e3e5d5501248bf2a1b148f03c
grifonna8 <grifonna8@gmail.com>
2019-12-16 13:29:27 +0300

    feat: added swiper and set media, changed structure html in col-50

--- js/main.js
+++ js/main.js
@@ -24,14 +24,27 @@ $(document).ready(function () {
     modal.toggleClass('modal--visible');
   });
 
+  // $(document).click(function (e) {
+  //   var container = $('.modal');
+  //   if (container.has(e.target).length === 0){
+  //       container.hide();
+  //   }
+  // });
+
+  // var code = e.keyCode;
+  
+  // if(code == 27) {
+  //   modal.toggleClass('modal--visible');
+  // });
+
   $(window).scroll(function () {
-    // Если отступ сверху больше 500px то показываем кнопку "Наверх"
-    if ($(this).scrollTop() > 500) {
+    // Если отступ сверху больше 750px то показываем кнопку "Наверх"
+    if ($(this).scrollTop() > 750) {
         $('.button--up').fadeIn();
     } else {
         $('.button--up').fadeOut();
     }
-});
+  });
 
 /** При нажатии на кнопку мы перемещаемся к началу страницы */
   $('.button--up').click(function () {
@@ -42,8 +55,23 @@ $(document).ready(function () {
   });
 
   var mySwiper = new Swiper ('.swiper-container', {
-    loop: true
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
   })
+
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  next.css('left', prev.width() + 10 + bullets.width() + 10)
+  bullets.css('left', prev.width() + 10)
 
 });
 
