7d52fffa2237c0084fab7866f5203d6562bf7f8d
grifonna8 <grifonna8@gmail.com>
2019-12-16 22:33:39 +0300

    refact: a try to make js code for modal window work again

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/* document.addEventListener("DOMContentLoaded", function(event) { 
+document.addEventListener("DOMContentLoaded", function(event) { 
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close');
@@ -10,68 +10,58 @@
   });
 
   closeBtn.addEventListener('click', switchModal);
-}); */
+});
 
-$(document).ready(function () {
-  var modal = $('.modal'),
-      modalBtn = $('[data-toggle="modal"]'),
-      closeBtn = $('.modal__close');
+// $(document).ready(function () {
+//   var modal = $('.modal'),
+//       modalBtn = $('[data-toggle="modal"]'),
+//       closeBtn = $('.modal__close');
   
-  modalBtn.on('click', function () {
-    modal.toggleClass('modal--visible');
-  });
-  closeBtn.on('click', function () {
-    modal.toggleClass('modal--visible');
-  });
+//   modalBtn.on('click', function () {
+//     modal.toggleClass('modal--visible');
+//   });
+//   closeBtn.on('click', function () {
+//     modal.toggleClass('modal--visible');
+//   });
 
-  // $(document).click(function (e) {
-  //   var container = $('.modal');
-  //   if (container.has(e.target).length === 0){
-  //       container.hide();
+  
+  
+
+  // $(window).scroll(function () {
+    // Если отступ сверху больше 750px то показываем кнопку "Наверх"
+  //   if ($(this).scrollTop() > 750) {
+  //       $('.button--up').fadeIn();
+  //   } else {
+  //       $('.button--up').fadeOut();
   //   }
   // });
 
-  // var code = e.keyCode;
-  
-  // if(code == 27) {
-  //   modal.toggleClass('modal--visible');
+/** При нажатии на кнопку мы перемещаемся к началу страницы */
+  // $('.button--up').click(function () {
+  //     $('body,html').animate({
+  //         scrollTop: 0
+  //     }, 900);
+  //     return false;
   // });
 
-  $(window).scroll(function () {
-    // Если отступ сверху больше 750px то показываем кнопку "Наверх"
-    if ($(this).scrollTop() > 750) {
-        $('.button--up').fadeIn();
-    } else {
-        $('.button--up').fadeOut();
-    }
-  });
+  // var mySwiper = new Swiper ('.swiper-container', {
+  //   loop: true,
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //     type: 'bullets',
+  //   },
+  //   navigation: {
+  //     nextEl: '.swiper-button-next',
+  //     prevEl: '.swiper-button-prev',
+  //   },
+  // });
 
-/** При нажатии на кнопку мы перемещаемся к началу страницы */
-  $('.button--up').click(function () {
-      $('body,html').animate({
-          scrollTop: 0
-      }, 900);
-      return false;
-  });
+  // var next = $('.swiper-button-next');
+  // var prev = $('.swiper-button-prev');
+  // var bullets = $('.swiper-pagination');
 
-  var mySwiper = new Swiper ('.swiper-container', {
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  })
-
-  var next = $('.swiper-button-next');
-  var prev = $('.swiper-button-prev');
-  var bullets = $('.swiper-pagination');
-
-  next.css('left', prev.width() + 10 + bullets.width() + 10)
-  bullets.css('left', prev.width() + 10)
+  // next.css('left', prev.width() + 20 + bullets.width() + 20)
+  // bullets.css('left', prev.width() + 20)
 
 });
 
