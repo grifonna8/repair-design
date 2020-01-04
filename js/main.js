617960f27d3d95d9ed7648162b5968b3fd8859df
grifonna8 <grifonna8@gmail.com>
2020-01-05 00:26:23 +0300

    a try to fix php sending

--- js/main.js
+++ js/main.js
@@ -245,6 +245,23 @@ $(document).ready(function () {
         myMap.behaviors.disable('scrollZoom');
   }); */
 
+  var player;
+  $('.video__play').on('click', function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      height: '465',
+      width: '100%',
+      videoId: 'MZqtJ1IrRNI',
+      events: {
+        'onReady': videoPlay,
+      }
+    });
+  })
+
+  function videoPlay(event) {
+    event.target.playVideo();
+  }
+    
+
 });
 
 
